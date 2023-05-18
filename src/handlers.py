23ac77910737d8cb2aaafa43241174f94bfdeff8
Aleksandr Menshikov <menshikov.a.v@icloud.com>
2023-05-18 15:48:29 +0300

    Правки кода

--- src/handlers.py
+++ src/handlers.py
@@ -84,7 +84,13 @@ async def handle_current_question(update: Update, context: ContextTypes.DEFAULT_
 
         return 'CURRENT_QUESTION'
 
-    if (path := Path(question.answer)) and path.is_file() and path.exists():
+    path = None
+    try:
+        path = Path(question.answer)
+    except OSError:
+        pass
+
+    if path and path.is_file() and path.exists():
         await context.bot.send_photo(
             photo=path.open(mode='rb'), chat_id=update.effective_chat.id, parse_mode=ParseMode.MARKDOWN_V2
         )
